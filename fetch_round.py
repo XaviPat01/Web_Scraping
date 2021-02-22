24b0c45374e42bde8d394ea784a4fc9a561a3943
XaviPat01 <patni.avi@gmail.com>
2021-02-22 12:13:34 +0530

    Handled the corner cases.

--- fetch_round.py
+++ fetch_round.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import os
+import ctypes
 #import time
 #from selenium.webdriver.common.by import By
 
@@ -15,40 +16,50 @@ def pset(a:str)->str:
     else:
         return a[:-1]
 
+
 cnum = input("Enter the contest number: ")
 
 url = "https://codeforces.com/problemset"
 driver = webdriver.Chrome()
 driver.get(url)
-found = False
+exitcond = False
 os.mkdir(f'{cnum}')
 pgnum=1
-while not found:
+while not exitcond:
     if driver.find_elements_by_xpath(f'//td[contains(@class,"id")]/a[text()[contains(.,{cnum})]]') and pset(driver.find_elements_by_xpath(f'//td[contains(@class,"id")]/a[text()[contains(.,{cnum})]]')[0].text)==cnum:
-        problemset=driver.find_elements_by_xpath(f'//td[contains(@class,"id")]/a[text()[contains(.,{cnum})]]')
-        found= True
-        namelist=[problem.text for problem in problemset]
-        #print(namelist)
-        plong=len(namelist)
-        for i in range(0,plong):
-            qname=pname(namelist[i])
-            folder=f'{cnum}/{qname}'
-            os.mkdir(folder)
-            urlnew = f'https://codeforces.com/problemset/problem/{cnum}/{qname}'
-            driver.get(urlnew)
-            driver.maximize_window()
-            qn = driver.find_element_by_xpath('//div[@class="problem-statement"]')
-            qn.screenshot(f'{folder}/problem.png')
+        j=0
+        while j<2:
+            driver.get(f'https://codeforces.com/problemset/page/{str(pgnum+j)}')
+            problemset=driver.find_elements_by_xpath(f'//td[contains(@class,"id")]/a[text()[contains(.,{cnum})]]')
+            exitcond= True
+            namelist=[problem.text for problem in problemset]
+            #print(namelist)
+            plong=len(namelist)
+            for i in range(0,plong):
+                qname=pname(namelist[i])
+                folder=f'{cnum}/{qname}'
+                os.mkdir(folder)
+                urlnew = f'https://codeforces.com/problemset/problem/{cnum}/{qname}'
+                driver.get(urlnew)
+                driver.maximize_window()
+                qn = driver.find_element_by_xpath('//div[@class="problem-statement"]')
+                qn.screenshot(f'{folder}/problem.png')
 
-            sampinput=driver.find_element_by_xpath('//div[@class="input"]').find_element_by_tag_name('pre').text
-            with open(f'{folder}/input.txt','w') as finput:
-                finput.write(sampinput)
+                sampinput=driver.find_element_by_xpath('//div[@class="input"]').find_element_by_tag_name('pre').text
+                with open(f'{folder}/input.txt','w') as finput:
+                    finput.write(sampinput)
 
-            sampoutput=driver.find_element_by_xpath('//div[@class="output"]').find_element_by_tag_name('pre').text
-            with open(f'{folder}/output.txt','w') as foutput:
-                foutput.write(sampoutput)
-            #driver.get(url)
-
+                sampoutput=driver.find_element_by_xpath('//div[@class="output"]').find_element_by_tag_name('pre').text
+                with open(f'{folder}/output.txt','w') as foutput:
+                    foutput.write(sampoutput)
+                #driver.get(url)
+            j+=1
+    elif int(pset(driver.find_element_by_xpath('//td[contains(@class,"id")]//a').text))<int(cnum):
+        exitcond=True
+        os.rmdir(f'{cnum}')
+        driver.quit()
+        print("The requested contest doesn't exist!")
+        #Error Box -----> ctypes.windll.user32.MessageBoxW(0, "The requested contest doesn't exist!", "Incorrect Contest Number",16)
     else:
         pgnum+=1
         urlnext = f'https://codeforces.com/problemset/page/{str(pgnum)}'
